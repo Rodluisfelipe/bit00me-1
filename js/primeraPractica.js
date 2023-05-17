9e14d1631bfa3c9c6b0a6121ef99fa011501cc2c
Sara Acuña <acunabenavidessaraelis@gmail.com>
2023-05-16 21:04:57 -0600

    clase js 2

--- js/primeraPractica.js
+++ js/primeraPractica.js
@@ -23,9 +23,9 @@ function modulo(num1, num2) {
     return num1 % num2;
 }
 
-// // Solicitar los dos números al usuario
-// let numero1 = parseFloat(prompt("Ingrese el No.1:"));
-// let numero2 = parseFloat(prompt("Ingrese el No.2:"));
+// Solicitar los dos números al usuario
+let numero1 = parseFloat(prompt("Ingrese el No.1:"));
+let numero2 = parseFloat(prompt("Ingrese el No.2:"));
 
 // RESULTADOS
 var resultadoSuma = suma(numero1, numero2);
@@ -34,21 +34,13 @@ var resultadoMultiplicacion = multiplicacion(numero1, numero2);
 var resultadoDivision = division(numero1, numero2);
 var resultadoModulo = modulo(numero1, numero2);
 
-// Calcular y mostrar el resultado en un mensaje de alerta
-// alert("Resultados de las operaciones:\n\n" +
-//       "Suma: " + resultadoSuma + "\n" +
-//       "Resta: " + resultadoResta + "\n" +
-//       "Multiplicación: " + resultadoMultiplicacion + "\n" +
-//       "Modulo: " + resultadoModulo + "\n" +
-//       "División: " + resultadoDivision+"\n"+
-//       "MUCHAS GRACIAS!!!"
-//     );
+alert("Resultados de las operaciones:\n\n" +
+      "Suma: " + resultadoSuma + "\n" +
+      "Resta: " + resultadoResta + "\n" +
+      "Multiplicación: " + resultadoMultiplicacion + "\n" +
+      "Modulo: " + resultadoModulo + "\n" +
+      "División: " + resultadoDivision+"\n"+
+      "MUCHAS GRACIAS!!!"
+);
 
 
-// inicio
-
-var nombre = prompt("Por favor, ingresa tu nombre:");
-var apellido = prompt("Por favor, ingresa tu apellido:");
-
-// Mostrar el nombre completo
-alert("Hola, " + nombre + " " + apellido + "! Bienvenido a mi este curso.");
