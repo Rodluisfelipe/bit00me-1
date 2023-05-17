240283851a5f32d447ba7e8d7df0da7ed76833a3
Sara Acuña <acunabenavidessaraelis@gmail.com>
2023-05-16 18:01:36 -0600

    aprendiendo js

---
+++ js/inicio.js
@@ -0,0 +1,38 @@
+"use strict"
+// modo estricto buena practica
+
+console.log("hola");
+
+// VARIABLES--->Nombres descriptivos
+var nombre = 'Madison';
+const edad = 16;
+let pais = "Colombia"
+
+// AMBITO
+// Let no es global sino por bloque de codigo
+//var es global
+
+// funciones
+function imprimirAño() {
+    var año = 2020;
+}
+
+// TIPO DE DATOS
+
+// Primitivos-->Fundametales de js
+let nombreCompleto = "Sara Acuña Benavides"
+console.log(typeof nombreCompleto);
+let edadNombre = 12;
+
+
+// CONVERSION DE TIPOS
+nombreCompleto = "23";
+console.log(typeof nombreCompleto);
+
+// convertir de string a numero
+var numero = 42;
+var cadena = numero.toString();
+console.log(cadena);
+
+// OPERADORES
+
