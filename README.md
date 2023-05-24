c7e4c978fe174c38392cae489a5560b563ffdbaa
Sara Acuña <acunabenavidessaraelis@gmail.com>
2023-05-24 03:26:28 -0600

    nuevo diseño presentacion

--- README.md
+++
@@ -1,4 +0,0 @@
-# bit00me
-# Titulo: MI PRESENTACION
-Descripcion: Actividad primera
-# Autor: SARA ACUÑA
---
+++ js/index.js
@@ -0,0 +1,14 @@
+function goToPage(url) {
+    window.location.href = url;
+}
+
+function zoomIn(element) {
+    element.style.transform = "scale(0.9)";
+  }
+  
+  function zoomOut(element) {
+    element.style.transform = "scale(1.0)";
+  }
+
+
+  
